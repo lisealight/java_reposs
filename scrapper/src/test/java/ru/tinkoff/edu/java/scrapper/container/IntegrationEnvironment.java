4ec7f3266a33d2b6837ada186d26c051a6c2f5c6
lisealight <lisaplehanova@mail.ru>
2023-04-12 16:00:05 +0500

    Правки

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/container/IntegrationEnvironment.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/container/IntegrationEnvironment.java
@@ -1,23 +1,14 @@
-package ru.tinkoff.edu.java.scrapper.container;
+package ru.tinkoff.edu.java.scrapper;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
-public abstract class IntegrationEnvironment {
+public class IntegrationEnvironment {
 
-    private static final PostgreSQLContainer<?> postgres;
+    protected static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
     static {
-        postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15"));
-        postgres.start();
-    }
-
-    @DynamicPropertySource
-    static void datasourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.datasource.username", postgres::getUsername);
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15");
+        POSTGRESQL_CONTAINER.withDatabaseName("scrapper");
+        POSTGRESQL_CONTAINER.start();
     }
 }
