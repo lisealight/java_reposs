f2a54fa17eec1e9a14c08d55cc2d670d3e9a1850
lisealight <lisaplehanova@mail.ru>
2023-05-18 15:22:25 +0500

    Exc

--- tinkoff_scrap_bot/bot/src/main/java/ru/tinkoff/edu/BotApplication.java
+++ tinkoff_scrap_bot/bot/src/main/java/ru/tinkoff/edu/BotApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.tinkoff.edu.configuration.ApplicationConfig;
+import ru.tinkoff.edu.model.Bot;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
@@ -13,5 +14,6 @@ public class BotApplication
     public static void main( String[] args )
     {
         SpringApplication.run(BotApplication.class, args);
+        new Bot("6276397167:AAELHBYnPozXqT3ewWhYF3jDWoCOAZ-YzNk");
     }
 }
